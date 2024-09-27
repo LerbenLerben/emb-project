7a36e1618adf15e8c95b3085fd0078ac65c29b94
LerbenLerben <57722101+LerbenLerben@users.noreply.github.com>
2024-09-27 13:20:56 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "stm32f4xx.h"
+
+void delay(uint32_t count) {
+    while(count--);
+}
+
+int main(void) {
+    // Включаем тактирование порта GPIOA
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+
+    // Настраиваем PA5 как выход
+    GPIOA->MODER |= GPIO_MODER_MODE5_0;
+
+    while (1) {
+        // Включаем светодиод
+        GPIOA->ODR |= GPIO_ODR_OD5;
+        delay(500000);
+
+        // Выключаем светодиод
+        GPIOA->ODR &= ~GPIO_ODR_OD5;
+        delay(500000);
+    }
+}
